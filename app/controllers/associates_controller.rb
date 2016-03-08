670d09c53dc7c5733c8e949417d043758587173e
Jeremy Gradisher <jeremygradisher@gmail.com>
2016-03-08 17:06:39 +0000

    Error, trying this again

--- app/controllers/associates_controller.rb
+++ app/controllers/associates_controller.rb
@@ -166,7 +166,6 @@ class AssociatesController < ApplicationController
     end
   end
   
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_associate
