28fdbbcc9674bce1a8f7bd02465d252ca2f18b16
jeremygradisher <jeremygradisher@gmail.com>
2021-02-12 16:40:28 +0000

    timeoutable added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :timeoutable, :timeout_in => 4.hours
 end
